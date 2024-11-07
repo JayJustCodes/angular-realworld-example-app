2c79ceb9a456e4715f1a522bfb0c41391f9fe2cd
jay <viccsey@gmail.com>
2024-11-07 22:30:19 +0100

    Adds discounts fetching to GoodsListComponent

--- src/app/shared/components/goods-list/goods-list.component.ts
+++ src/app/shared/components/goods-list/goods-list.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit, DestroyRef } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { GoodsItemComponent } from "../goods-item/goods-item.component";
 import { GoodsItemsService } from "../../services/goods-items.service";
+import { DiscountsService } from "../../services/discounts.service";
+import { type Discounts } from "../../models/discounts.model";
 import { type GoodsItem } from "../../models/goods-item.model";
 
 @Component({
@@ -14,22 +16,42 @@ import { type GoodsItem } from "../../models/goods-item.model";
 export class GoodsListComponent implements OnInit {
     private readonly goodsService = inject(GoodsItemsService);
 
+    private readonly discountsService = inject(DiscountsService);
+
     private readonly destroyRef = inject(DestroyRef);
 
     sellingItemsList: GoodsItem[] = [];
 
+    discountsList: Discounts[] = [];
+
     ngOnInit(): void {
         this.loadGoodsItems();
+        this.loadDiscounts();
     }
 
     private loadGoodsItems(): void {
         const subscription = this.goodsService.getGoodsItems().subscribe(
-            (data) => {
+            (data: GoodsItem[]) => {
                 this.sellingItemsList = data;
                 console.log(this.sellingItemsList);
             },
-            (error) => {
+            (error: any) => {
                 console.error("Error fetching goods items", error);
+            },
+        );
+        this.destroyRef.onDestroy(() => {
+            subscription.unsubscribe();
+        });
+    }
+
+    private loadDiscounts(): void {
+        const subscription = this.discountsService.getDiscounts().subscribe(
+            (data: Discounts[]) => {
+                this.discountsList = data;
+                console.log("discounts", this.discountsList);
+            },
+            (error: any) => {
+                console.error("Error fetching discounts", error);
             },
         );
         this.destroyRef.onDestroy(() => {
