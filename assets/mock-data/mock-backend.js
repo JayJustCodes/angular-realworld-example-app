3965fef835079c8f8fdcf7f2b1aeead8870b9183
jay <viccsey@gmail.com>
2024-11-13 12:34:32 +0100

    Simulate server response delay for discounts with a 500ms delay

--- assets/mock-data/mock-backend.js
+++ assets/mock-data/mock-backend.js
@@ -4,27 +4,30 @@ const path = require("path");
 
 const PORT = 3000;
 
-function serveJsonFile(filePath, res) {
-    fs.readFile(filePath, "utf-8", (err, data) => {
-        if (err) {
-            res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "Failed to read file" }));
-        } else {
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(data);
-        }
-    });
+function serveJsonFile(filePath, res, delay = 0) {
+    setTimeout(() => {
+        fs.readFile(filePath, "utf-8", (err, data) => {
+            if (err) {
+                res.writeHead(500, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ error: "Failed to read file" }));
+            } else {
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.end(data);
+            }
+        });
+    }, delay);
 }
 
 const server = http.createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     if (req.method === "GET") {
         if (req.url === "/items") {
             serveJsonFile(path.join(__dirname, "items.json"), res);
         } else if (req.url === "/discounts") {
-            serveJsonFile(path.join(__dirname, "discounts.json"), res);
+            serveJsonFile(path.join(__dirname, "discounts.json"), res, 500);
         } else {
             res.writeHead(404, { "Content-Type": "application/json" });
             res.end(JSON.stringify({ error: "Not found" }));
