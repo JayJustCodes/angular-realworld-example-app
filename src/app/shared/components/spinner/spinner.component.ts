f2239fbce080ff4e6137e58b8f23f43df53c4f8a
jay <viccsey@gmail.com>
2024-11-23 18:25:56 +0100

    Refactors SpinnerComponent to use LoadingState

--- src/app/shared/components/spinner/spinner.component.ts
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { LoadingState } from "../../models/loading-state.model";
 import { LoadingService } from "../../services/loading.service";
 
 @Component({
@@ -7,20 +8,28 @@ import { LoadingService } from "../../services/loading.service";
     standalone: true,
     imports: [CommonModule],
     template: `
-        <div class="spinner-overlay" *ngIf="isLoading">
+        <div class="spinner-overlay" *ngIf="loadingStateValue?.status == 'loading'">
             <div class="spinner"></div>
         </div>
     `,
     styleUrl: "./spinner.component.css",
 })
 export class SpinnerComponent implements OnInit {
-    isLoading = false;
+    private loadingState: LoadingState | null = null;
 
     constructor(private loadingService: LoadingService) {}
 
-    ngOnInit() {
-        this.loadingService.loading$.subscribe((loading) => {
-            this.isLoading = loading;
+    private setLoadingState(state: LoadingState): void {
+        this.loadingState = state;
+    }
+
+    public get loadingStateValue(): LoadingState | null {
+        return this.loadingState;
+    }
+
+    public ngOnInit(): void {
+        this.loadingService.getLoading().subscribe((state: LoadingState) => {
+            this.setLoadingState(state);
         });
     }
 }
