ca5ea411c8f2840aee1ad97bf5b3fb4abd695e99
jay <viccsey@gmail.com>
2024-11-28 13:08:33 +0100

    Adds route parameter parsing to ItemDetails component

--- src/app/pages/item-details/item-details.component.ts
+++ src/app/pages/item-details/item-details.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-item-details',
-  standalone: true,
-  imports: [],
-  templateUrl: './item-details.component.html',
-  styleUrl: './item-details.component.css'
+    selector: "app-item-details",
+    standalone: true,
+    imports: [],
+    templateUrl: "./item-details.component.html",
+    styleUrl: "./item-details.component.css",
 })
 export class ItemDetailsComponent {
+    private itemId!: number;
 
+    constructor(private route: ActivatedRoute) {}
+
+    public ngOnInit(): void {
+        const idParam: string | null = this.route.snapshot.paramMap.get("id")!;
+        if (idParam) {
+            this.itemId = Number(idParam);
+        }
+        console.log("id", this.itemId);
+    }
 }
