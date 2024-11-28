16427d89cfaa4eec93784749df000bf5cd794592
jay <viccsey@gmail.com>
2024-11-28 10:35:20 +0100

    Creates ItemDetails page templates and its routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { ContactPageComponent } from "./pages/contact-page/contact-page.component";
+import { ItemDetailsComponent } from "./pages/item-details/item-details.component";
 
 export const routes: Routes = [
     {
@@ -12,5 +13,10 @@ export const routes: Routes = [
         path: "contact-page",
         component: ContactPageComponent,
         title: "Contact Page",
+    },
+    {
+        path: "item-details/:id",
+        component: ItemDetailsComponent,
+        title: "Item Details",
     },
 ];
